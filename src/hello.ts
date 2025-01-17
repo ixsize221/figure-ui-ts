2172b95edf95c17f70776448509c0b0e8f261ead
CodeSandbox <support@codesandbox.io>
2025-01-17 10:28:34 +0000

    Sandbox snapshot commit

--- src/hello.ts
+++ src/hello.ts
@@ -1,2 +1,29 @@
-console.log("Привет, студенты!");
-console.log("Пр3335т, студенты!");
+console.log("Привет, Вася!");
+let a: number = 10;
+console.log(a);
+
+const myName: string = "John";
+let isOK: boolean;
+let array: number[];
+let strArray: Array<string>;
+
+isOK = true;
+array = [1, 2, 3];
+strArray = ["a", "b", "c"];
+console.log(isOK);
+console.log(array);
+console.log(strArray);
+
+function square(num: number): number {
+  return num * num;
+}
+let result = square(10);
+
+function printId(id: number | string): void {
+  console.log(`Ваш ID: ${id}`);
+}
+printId(3789);
+printId("#678ad");
+
+function move(from: object, howLong: number) {}
+function move(from: Point, howLong: Distance) {}
