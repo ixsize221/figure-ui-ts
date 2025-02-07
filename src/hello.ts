6f014aa37b579f2412a633b7f2be82e6cb72092b
CodeSandbox <support@codesandbox.io>
2025-02-07 10:40:04 +0000

    Sandbox snapshot commit

--- src/hello.ts
+++ src/hello.ts
@@ -14,21 +14,66 @@ class Point {
 }
 
 class Rectangle {
-  private x1: number;
-  private y1: number;
-  private x2: number;
-  private y2: number;
+  private _x1: number;
+  private _y1: number;
+  private _x2: number;
+  private _y2: number;
+
   constructor(x1: number, y1: number, x2: number, y2: number) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+    if (x1 >= 0 && x2 >= 0 && y1 >= 0 && y2 >= 0) {
+      this._x1 = x1;
+      this._y1 = y1;
+      this._x2 = x2;
+      this._y2 = y2;
+    } else {
+      throw new Error("Координаты должны быть >=0");
+    }
+  }
+  set x1(num: number) {
+    if (num >= 0) {
+      this._x1 = num;
+    } else {
+      throw new Error("Значение х1 отклонено");
+    }
+  }
+  get x1(): number {
+    return this._x1;
+  }
+  get x2(): number {
+    return this._x2;
+  }
+  get y1(): number {
+    return this._y1;
+  }
+  get y2(): number {
+    return this._y2;
+  }
+  set x2(num: number) {
+    if (num >= 0) {
+      this._x2 = num;
+    } else {
+      throw new Error("Значение х2 отклонено");
+    }
+  }
+  set y1(num: number) {
+    if (num >= 0) {
+      this._y1 = num;
+    } else {
+      throw new Error("Значение y1 отклонено");
+    }
+  }
+  set y2(num: number) {
+    if (num >= 0) {
+      this._y2 = num;
+    } else {
+      throw new Error("Значение y2 отклонено");
+    }
   }
   get длинна(): number {
-    return Math.abs(this.x2 - this.x1);
+    return Math.abs(this._x2 - this._x1);
   }
   get высота(): number {
-    return Math.abs(this.y2 - this.y1);
+    return Math.abs(this._y2 - this._y1);
   }
   get периметр(): number {
     return this.высота * 2 + this.длинна * 2;
@@ -42,32 +87,38 @@ class Rectangle {
   }
 }
 let квадрат1 = new Rectangle(0, 0, 10, 10);
-let квадрат2 = new Rectangle(30, 0, -2000, 1000);
-// квадрат2.x1 = -99999;
+let квадрат2 = new Rectangle(30, 0, 2000, 1000);
+квадрат2.x1 = 999;
+квадрат2.x2 = 0;
+квадрат2.y1 = 50;
+квадрат2.y2 = 1000;
 console.log(квадрат2);
 console.log(квадрат2.длинна);
 console.log(квадрат2.info);
 console.log(квадрат1.info);
 
-https://codesandbox.io/p/devbox/igorb-ts-blank-forked-3hkl2q
-class Rect {
-  startPoint: Point;
-  width: number;
-  height: number;
-  constructor(?????????){
-    this.???????? = ??????
-    this.???????? = ??????
-    this.???????? = ??????
+class Tringle {
+  private _a: Point;
+  private _b: Point;
+  private _c: Point;
+  constructor(a: Point, b: Point, c: Point) {
+    this._a = a;
+    this._b = b;
+    this._c = c;
   }
-  get площадь():number{}
-  get периметр():number{}
-  get endPoint():Point{}
+  get a() {
+    return this._a;
+  }
+  get b() {
+    return this._b;
+  }
+  get c() {
+    return this._c;
+  }
+  get square() {
+    return num;
+  }
 }
-
-https://codesandbox.io/p/devbox/igorb-ts-blank-forked-3hkl2q
-
-
-
 
 // console.log("Привет, Вася!");
 // let a: number = 10;
