7d86354abf8ff9fcec9bd261d3818a368d266e38
CodeSandbox <support@codesandbox.io>
2025-03-04 10:29:26 +0000

    Sandbox snapshot commit

--- src/mebel.ts
+++ src/mebel.ts
@@ -1,12 +1,14 @@
+// https://codesandbox.io/p/devbox/kh-12-points-3hkl2q
+
 class Мебель {
-  protected name: string;
+  name: string;
   size: [number, number, number];
-  protected _price: number;
+  protected price: number;
   constructor(name: string, size: string, price: number) {
     this.name = name;
     let arr = size.split("х").map(Number);
     if (arr.length === 3) this.size = [arr[0], arr[1], arr[2]];
-    this._price = price;
+    this.price = price;
   }
 }
 
@@ -35,7 +37,7 @@ console.log(стулПервый, кожаныйСтул);
 
 class Стол extends Мебель {
   private _ножки: number;
-  private _площадьСтолешницы: number;
+  площадьСтолешницы: number;
   constructor(
     названиеСтола: string,
     цена: number,
@@ -47,24 +49,21 @@ class Стол extends Мебель {
     this.площадьСтолешницы = площадьСтолешницы;
   }
   set ножки(num: number) {
-    if (num > 0 && num <= 10) this._ножки = num;
-    else throw new Error("Количество ножек не правильное");
+    if (num > 0 && num < 20) this._ножки = num;
+    else throw new Error("количество ножек не праивльное");
   }
-  set площадьСтолешницы(square: number) {
-    this._площадьСтолешницы = square;
-    if (square >= 0 && square <= 100000) this._площадьСтолешницы = square;
-    else throw new Error("Площадь не правильная");
+  get ножки() {
+    return this._ножки;
   }
-  set price(num: number) {
-    if (num >= 0 && num < 10000) this._price = num;
+  скидка() {
+    console.log(`скидка на стол ${this.price * 0.2} рублей `);
   }
 }
 
 let класныйСтол1 = new Стол("Из цельного бруса", 25000, 2000, 3);
 let кухня1 = new Стол("Кухонный стол", 14000, 3000);
-класныйСтол1.ножки = 5; // так работает
-//класныйСтол1.ножки = 500; // а так не работает, не проходит проверку кол = 0 -- 10 на сеттере
-класныйСтол1.price = 333;
+класныйСтол1.ножки = 12; // так работает
+//класныйСтол1.ножки = -4444; // а так не работает, не проходит проверку кол = 0 -- 10 на сеттере
 
 класныйСтол1.площадьСтолешницы = 3333;
 console.log(класныйСтол1, кухня1);
