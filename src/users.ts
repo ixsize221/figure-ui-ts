026560ea3f437a224c945061b4423fcbdd805d9a
CodeSandbox <support@codesandbox.io>
2025-03-14 10:30:07 +0000

    Sandbox snapshot commit

--- src/users.ts
+++ src/users.ts
@@ -1,34 +1,31 @@
-class User{
-    public name:string
-    protected _login:string
-    protected _password:string
-    protected _grade:number
-    constructor(name,login,pass,grade){
-        this.name = name;
-        this._login = login
-        this.grade = grade
-    }
-    set grade(num){        
-        if(num>0)
-            this._grade = num
-        else
-            console.log("grade error")
-    }
-    get login(){
-        return this._login
-    }
-    set login(str){
-        console.log("Логин нельзя изменить")
-    }
-    set password(str){
-        this._password = str
-    }
-    get password(){
-        return "*********"
-    }
+class User {
+  public name: string;
+  protected _login: string;
+  protected _password: string;
+  protected grade: number;
+  constructor(name: string, login: string, pass: string, grade: number) {
+    this.name = name;
+    this._login = login;
+    this._password = pass;
+    if (grade > 0) this.grade = grade;
+    else console.log("grade error");
+  }
+
+  get login() {
+    return this._login;
+  }
+  set login(str) {
+    console.log("Логин нельзя изменить");
+  }
+  set password(str) {
+    this._password = str;
+  }
+  get password() {
+    return "*********";
+  }
 }
 
-const user1 = new User('Paul McCartney', 'paul', '1234', 3)
-const user2 = new User('George Harrison', 'george', '5678', 2)
-const user3 = new User('Richard Starkey', 'ringo', '8523', 3)
-console.log(user1,user2);
+const user1 = new User("Paul McCartney", "paul", "1234", 3);
+const user2 = new User("George Harrison", "george", "5678", 2);
+const user3 = new User("Richard Starkey", "ringo", "8523", 3);
+console.log(user1, user2);
