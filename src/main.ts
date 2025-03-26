9c3eaae3e1cdc75746a6ea41ce6bf7f421479e1c
CodeSandbox <support@codesandbox.io>
2025-03-26 19:01:18 +0000

    Sandbox snapshot commit

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-// require("./src/area");
-document.getElementById("app")!.innerHTML = `
-  <h1>Hello Vite2222!</h1>
-  <p>Если это видно - Vite работает</p>
-`;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  root: ".",
-  publicDir: "../public",
+  root: ".", // Корень проекта
+  publicDir: "./public",
   build: {
     outDir: "./dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/ts/main.ts"), // Явный путь к входному файлу
+    },
   },
   server: {
-    open: true, // Автоматически открывать браузер
+    host: "0.0.0.0",
+    port: 5173,
+    open: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/ts"), // Алиас для удобных импортов
+    },
   },
 });
