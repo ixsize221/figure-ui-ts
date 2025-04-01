d115c93928b70e9454653621302f9ac545f1e78a
CodeSandbox <support@codesandbox.io>
2025-04-01 10:54:08 +0000

    Sandbox snapshot commit

--- src/ts/classes/area.ts
+++ src/ts/classes/area.ts
@@ -1,59 +1,58 @@
 class Точка {
-  private x: number;
-  private y: number;
+  private x: number
+  private y: number
   constructor(x: number, y: number) {
     // ЕСЛИ ТОЧКА Х и У положительные > 0, то все ок, иначе ошибка
     if (x >= 0 && y >= 0) {
-      this.x = x;
-      this.y = y;
+      this.x = x
+      this.y = y
     } else {
-      throw new Error("Без отрицательных чисел плес");
+      throw new Error('Без отрицательных чисел плес')
     }
   }
 }
 
 abstract class Фигура {
-  public center;
+  public center
   constructor(center: Точка) {
-    this.center = center;
+    this.center = center
   }
-  abstract площадь(): number;
+  abstract площадь(): number
 }
 
 class Круг extends Фигура {
-  public radius; // радиус в метрах
+  public radius // радиус в метрах
   constructor(radius: number) {
     // на супер конструктор уходит центр фигуры -- точка 0, 0
-    super(new Точка(0, 0));
-    this.radius = radius;
+    super(new Точка(0, 0))
+    this.radius = radius
   }
   площадь(): number {
-    return Math.PI * this.radius * this.radius;
+    return Math.PI * this.radius * this.radius
   }
   диаметр(): number {
-    return this.radius * 2;
+    return this.radius * 2
   }
   периметр(): number {
-    return 2 * Math.PI * this.radius;
+    return 2 * Math.PI * this.radius
   }
-  
 }
 
 class Прямоугольник extends Фигура {
-  public width: number; // ширина в метрах
-  public height: number; // высота в метрах
+  public width: number // ширина в метрах
+  public height: number // высота в метрах
   constructor(width: number, height: number) {
     // на супер конструктор уходит центр фигуры -- точка 0, 0
-    super(new Точка(0, 0));
-    this.height = height;
-    this.width = width;
+    super(new Точка(0, 0))
+    this.height = height
+    this.width = width
   }
   площадь(): number {
-    return this.width * this.height;
+    return this.width * this.height
   }
   периметр(): number {
-    return this.height * 2 + this.width * 2;
+    return this.height * 2 + this.width * 2
   }
 }
 
-export { Точка, Фигура, Круг, Прямоугольник };
+export { Точка, Фигура, Круг, Прямоугольник }
